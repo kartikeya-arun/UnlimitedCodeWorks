4a91f17873e043ac9c46fdcf150ac8682e9bfeb7
Kartikeya <kartikeya7.96@gmail.com>
2021-02-15 00:45:47 +0530

    Fixed Footer

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -7,7 +7,7 @@ export default function Footer(){
             <p>Contact me on these platforms</p>
             <ul>
                 <li><a id='linkedin' href="https://www.linkedin.com/in/kartikeya-arun/"><i class="fa fa-linkedin-square fa-2x"></i></a></li>
-                <li><a id='github' href="https://github.com/Watch-D0GS"><i class="fa fa-github-square fa-2x"></i></a></li>
+                <li><a id='github' href="https://github.com/kartikeya-arun"><i class="fa fa-github-square fa-2x"></i></a></li>
                 <li><a id='codepen' href="https://codepen.io/c-h-o-c-o-l-a-t-e"><i class="fa fa-codepen fa-2x"></i></a></li>
                 <li><a id='instagram' href="https://www.instagram.com/kartikeya_arun/"><i class="fab fa-instagram-square fa-2x"></i></a></li>
             </ul>
